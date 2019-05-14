f441914c68b2062ed0e0ad0cb5dd753d2d34abee
jiangxing110 <1570205314@qq.com>
2019-05-14 21:58:35 +0800

    dd

--- src/main/java/com/zhiyun/readwrite/service/Impl/ApiServiceImpl.java
+++ src/main/java/com/zhiyun/readwrite/service/Impl/ApiServiceImpl.java
@@ -1,12 +1,16 @@
 package com.zhiyun.readwrite.service.Impl;
 
 import com.alibaba.fastjson.JSON;
+import com.rabbitmq.client.AMQP;
 import com.zhiyun.readwrite.config.Constants;
 import com.zhiyun.readwrite.entity.SystemTask;
 import com.zhiyun.readwrite.service.ApiService;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.amqp.rabbit.core.RabbitAdmin.QUEUE_NAME;
 
 /**
  * @Title: ApiServiceImpl
@@ -25,10 +29,11 @@ public class ApiServiceImpl implements ApiService {
 
     @Override
     public String sendMessage(SystemTask systemTask) {
-
+        AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder().expiration("500").build();
+        //senderChannel.basicPublish("", QUEUE_NAME, properties, message.getBytes("UTF-8"));
 
         //rabbitTemplate.convertAndSend(Constants.DF_SYSTEM_TASK_EXCHANGE_NAME,"com.df.qwqw",JSON.toJSONString(systemTask));
-        rabbitTemplate.convertAndSend(Constants.DF_SYSTEM_TASK_EXCHANGE_NAME,"com.df.qwqwwwww",JSON.toJSONString(systemTask));
+        rabbitTemplate.convertAndSend(Constants.DF_SYSTEM_TASK_EXCHANGE_NAME,"com.df.qwqwwwww",JSON.toJSONString(systemTask), (MessagePostProcessor) properties);
         return systemTask.toString();
     }
 }
