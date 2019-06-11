5dd3e3dd5b57cec0ad3687a2c861216f8d266297
jiangxing140@163.com <jiangxing140@163.com>
2019-06-11 16:31:06 +0800

    信息

---
+++ src/main/java/com/zhiyun/readwrite/controller/MongodbRepositoryController.java
@@ -0,0 +1,9 @@
+package com.zhiyun.readwrite.controller;/**
+ * @Title: MongodbRepositoryController
+ * @ProjectName: readwrite
+ * @Description: Repository操作
+ * @author: jiangxing
+ * @date 2019/6/1116:13
+ */
+public class MongodbRepositoryController {
+}
